26358c13a3f7449772d6fee11334bf5944f0822b
shefaitur <shefaitur@gmail.com>
2023-03-15 00:17:55 +0600

    all file update

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,20 +1,94 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { Rings } from "react-loader-spinner";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FcGoogle } from "react-icons/fc";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 const Login = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  let navigate = useNavigate();
+  let [email, setEmail] = useState("");
+  let [erremail, setErremail] = useState("");
+  let [password, setPassword] = useState("");
+  let [errpassword, setErrPassword] = useState("");
+  let [loader, setLoader] = useState(true);
+  let [loginsuc, setLoginsuc] = useState("");
+  let [eay, setEay] = useState(true);
+  let handelemail = (e) => {
+    setEmail(e.target.value);
+    setErremail("");
+  };
+  let handelpassword = (e) => {
+    setPassword(e.target.value);
+    setErrPassword("");
+  };
+  let handelsignin = () => {
+    if (!email) {
+      setErremail("please Enter Your Email");
+    }
+    if (!password) {
+      setErrPassword("Enter Your Password");
+    }
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        setLoginsuc("SignIn Successfuly");
+        toast("Login Successful");
+        setLoader(false);
+        setTimeout(() => {
+          navigate("/home");
+          setLoader(true);
+        }, 3000);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode, "..errorcode");
+        if (errorCode.includes("auth/wrong-password")) {
+          setErrPassword("password Not Correct");
+        }
+        if (errorCode.includes("auth/invalid-email")) {
+          setErremail("Not valid-email");
+        }
+        if (errorCode.includes("auth/user-not-found")) {
+          setErremail("Not Register Your Email");
+        }
+      });
+  };
+  let handelglogin=()=>{
+    signInWithPopup(auth, provider).then(()=>{
+      navigate("/home");
+    })
+  }
+  let handelEay = () => {
+    if (eay) {
+      setEay(false);
+    } else {
+      setEay(true);
+    }
+  };
   return (
     <div className="flex">
       <div className="w-1/2 flex justify-end bg-page pt-56">
         <div>
+          <ToastContainer />
           <h1 className="text-white text-4xl mr-20  font-nuni font-bold">
-            Get started with easily register
+            Login to your account!
           </h1>
-          <h2 className="text-white text-xl mr-20 mt-2.5 font-nuni font-regular">
-            Free register and you can enjoy it
-          </h2>
+          <div onClick={handelglogin} className="relative border py-2 w-[50%] mt-2 bg-primary rounded-lg">
+            <h2 className=" text-white text-xl  mt-2.5 ml-10 font-nuni font-regular">
+              Login with Google
+            </h2>
+            <FcGoogle className="absolute top-4 left-0 text-3xl" />
+          </div>
 
+          <h2 className="text-white text-2xl bg-primary mr-20 mt-2.5 font-nuni font-regular">
+            {loginsuc}
+          </h2>
           <div className="relative w-96 border border-primary border-2px rounded-[10px] mt-14">
             <input
+              onChange={handelemail}
               className="w-full p-[26px] rounded-[10px] text-primary text-xl font-nuni font-semibold  outline-none"
               type="email"
             />
@@ -22,24 +96,60 @@ const Login = () => {
               Email Addres
             </h3>
           </div>
+          <h3 className="bg-red-500 w-96 mt-1.5 rounded-[10px]  px-8 text-white text-sm font-nuni font-sm ">
+            {erremail}
+          </h3>
 
           <div className="relative w-96 border border-primary border-2px rounded-[10px] mt-14">
             <input
+              onChange={handelpassword}
               className="w-full p-[26px] rounded-[10px] text-primary text-xl font-nuni font-semibold outline-none"
-              type="password"
+              type={eay ? "password" : "text"}
             />
             <h3 className="absolute top-[-13px] left-[50px] bg-page h-[13px] px-8 text-[#11175D] text-sm font-nuni font-semibold ">
               Password
             </h3>
+            <div
+              onClick={handelEay}
+              className="absolute top-[30px] right-[20px]"
+            >
+              {eay ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+            </div>
           </div>
-          <button className="w-96 bg-primary text-white py-6 mt-12 rounded-full text-xl font-nuni font-semibold">
-            Sign up
-          </button>
+          <h3 className="bg-red-500 w-96 mt-1.5 rounded-[10px]  px-8 text-white text-sm font-nuni font-sm ">
+            {errpassword}
+          </h3>
+
+          {loader ? (
+            <button
+              onClick={handelsignin}
+              className="w-96 bg-primary text-white py-6 mt-12 rounded-full text-xl font-nuni font-semibold"
+            >
+              Sign in
+            </button>
+          ) : (
+            <div className="w-96 pl-32 mt-12">
+              <Rings
+                height="90"
+                width="90"
+                color="#844F50"
+                radius="6"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="rings-loading"
+              />
+            </div>
+          )}
+
           <p className="mt-8 ml-20 text-sm font-nuni font-regular text-white">
             Don’t have an account ?
             <span className="font-bold text-primary">
               <Link to="/registration">Sign up</Link>
-            </span>{" "}
+            </span>
+          </p>
+          <p className="mt-2 ml-28 text-sm font-nuni font-bold text-primary">
+            Forget Password
           </p>
         </div>
       </div>
--- src/pages/Ragistration/index.js
+++ src/pages/Ragistration/index.js
@@ -1,135 +1,168 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import {BsFillEyeFill,BsFillEyeSlashFill} from 'react-icons/bs'
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { Link, useNavigate } from "react-router-dom";
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import {getAuth,createUserWithEmailAndPassword,sendEmailVerification,} from "firebase/auth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Rings } from "react-loader-spinner";
 const Ragistration = () => {
   const auth = getAuth();
-  let [email,setEmail] = useState ("");
-  let [erremail,setErremail] = useState ("");
-  let [name,setName] = useState ("");
-  let [errname,setErrname] = useState ("");
-  let [password, setPassword] = useState ("");
-  let [errpassword, setErrpassword] = useState ("");
-  let [eay, setEay] = useState (true);
-
+  let navigate = useNavigate();
+  let [email, setEmail] = useState("");
+  let [erremail, setErremail] = useState("");
+  let [name, setName] = useState("");
+  let [errname, setErrname] = useState("");
+  let [password, setPassword] = useState("");
+  let [errpassword, setErrpassword] = useState("");
+  let [eay, setEay] = useState(true);
+  let [regsuc, setRegsuc] = useState("");
+  let [loader, setLoader] = useState(true);
   let handelEmail = (e) => {
     setEmail(e.target.value);
-    setErremail("")
-    
+    setErremail("");
   };
 
   let handelName = (e) => {
     setName(e.target.value);
-    setErrname("")
+    setErrname("");
   };
 
   let handelPassword = (e) => {
     setPassword(e.target.value);
-    setErrpassword("")
+
+    setErrpassword("");
   };
-  let handelSubmit =()=>{
-    if(!email){
-      setErremail("Email is requard")
-    }else{
-      if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)){
-        setErremail("Invalid Emali!!")
-      
+  let handelSubmit = () => {
+    if (!email) {
+      setErremail("Email is requard");
+    } else {
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)) {
+        setErremail("Invalid Emali!!");
       }
-     
     }
-    if(!name){
-      setErrname("Name is requard")
+    if (!name) {
+      setErrname("Name is requard");
     }
-     if(!password){
-      setErrpassword("Password is requard")
+    if (!password) {
+      setErrpassword("Password is requard");
     }
 
     createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    const user = userCredential.user;
- console.log("registration Sucsses")
-    
-  })
-  .catch((error) => {
-    const errorCode = error.code;   
-    const errorMessage = error.message;
-    // ..
-  });
-   
-  }
+      .then(() => {
+        setRegsuc("registration successfully.Please verify your email for your security");
+        toast("Please verify your email for your security");
+        setEmail("");
+        setName("");
+        setPassword("");
+        setLoader(false);
+        sendEmailVerification(auth.currentUser);
+        setTimeout(() => {
+          navigate("/login");
+          setLoader(true);
+        }, 4000);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        // ..
+      });
+  };
 
-  let handelEay =()=>{
-    if(eay){
-      setEay(false)
-    }else{
-      setEay(true)
+  let handelEay = () => {
+    if (eay) {
+      setEay(false);
+    } else {
+      setEay(true);
     }
-  }
+  };
 
   return (
     <div className="flex">
       <div className="w-1/2 flex justify-end bg-page pt-48">
         <div>
+          <ToastContainer />
           <h1 className="text-white text-4xl mr-20  font-nuni font-bold">
             Get started with easily register
           </h1>
           <h2 className="text-white text-xl mr-20 mt-2.5 font-nuni font-regular">
             Free register and you can enjoy it
           </h2>
-    
+          <h2 className="text-white text-2xl bg-primary mr-20 mt-2.5 font-nuni font-regular">
+            {regsuc}
+          </h2>
+
           <div className="relative w-96 border border-primary border-2px rounded-[10px] mt-14">
             <input
               onChange={handelEmail}
               className="w-full p-[26px] rounded-[10px] text-primary text-xl font-nuni font-semibold  outline-none"
               type="email"
+              value={email}
             />
             <h3 className="absolute top-[-13px] left-[50px] bg-page h-[13px] px-8 text-[#11175D] text-sm font-nuni font-semibold">
               Email Addres
             </h3>
-            
           </div>
-          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">{erremail}</h1>
+          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">
+            {erremail}
+          </h1>
 
           <div className="relative w-96 border border-primary border-2px rounded-[10px] mt-14">
             <input
               onChange={handelName}
               className="w-full p-[26px] rounded-[10px] text-primary text-xl font-nuni font-semibold outline-none"
               type="text"
+              value={name}
             />
             <h3 className="absolute top-[-13px] left-[50px] bg-page h-[13px] px-8 text-[#11175D] text-sm font-nuni font-semibold">
               Full Name
             </h3>
           </div>
-          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">{errname}</h1>
+          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">
+            {errname}
+          </h1>
 
           <div className="relative w-96 border border-primary border-2px rounded-[10px] mt-14">
             <input
-            onChange={handelPassword}
+              onChange={handelPassword}
               className="w-full p-[26px] rounded-[10px] text-primary text-xl font-nuni font-semibold outline-none"
-             
-                type={eay?"password":"text"}
-               
-              
+              type={eay ? "password" : "text"}
+              value={password}
             />
             <h3 className="absolute top-[-13px] left-[50px] bg-page h-[13px] px-8 text-[#11175D] text-sm font-nuni font-semibold ">
               Password
             </h3>
-            <div onClick={handelEay} className="absolute top-[30px] right-[20px]">
-              {eay
-              ?
-              <BsFillEyeSlashFill/>
-              :
-              <BsFillEyeFill/>
-              }
-              
-             
+            <div
+              onClick={handelEay}
+              className="absolute top-[30px] right-[20px]"
+            >
+              {eay ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
             </div>
           </div>
-          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">{errpassword}</h1>
-          <button onClick={handelSubmit} className="w-96 bg-primary text-white py-6 mt-12 rounded-full text-xl font-nuni font-semibold">
-            Sign up
-          </button>
+          <h1 className="w-96 bg-red-400 text-white text-sm font-nuni font-semibold rounded-[10px] mt-1 text-center">
+            {errpassword}
+          </h1>
+          
+          {loader ? (
+            <button
+              onClick={handelSubmit}
+              className="w-96 bg-primary text-white py-6 mt-12 rounded-full text-xl font-nuni font-semibold"
+            >
+              Sign up
+            </button>
+          ) : (
+            <div className="w-96 pl-32 mt-12">
+              <Rings
+                height="90"
+                width="90"
+                color="#844F50"
+                radius="6"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="rings-loading"
+              />
+            </div>
+          )}
+
           <p className="mt-8 ml-20 text-sm font-nuni font-regular text-white">
             Already have an account ?
             <span className="font-bold text-primary">
