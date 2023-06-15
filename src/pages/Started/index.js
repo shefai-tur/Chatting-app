297b8e247e3f2d22e0f19ab1b126488f4925c437
shefaitur <121374560+shefai-tur@users.noreply.github.com>
2023-06-15 22:41:35 +0600

    title update

--- src/pages/Started/index.js
+++ src/pages/Started/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 const Started = () => {
   return (
     <div className="bg-page ">
@@ -13,15 +13,17 @@ const Started = () => {
       </image>
       <div className="bg-primary ml-2.5 rounded-t-[20%]">
         <h1 className=" pt-20 text-3xl text-white text-center font-nuni font-black	">
-          Welcome to “HiChat”
+          Welcome to “College-Connect”
         </h1>
-        <h2 className="mx-auto text-2xl text-white text-center font-nuni font-medium mt-8 w-60">The place where you will find friends to chat with</h2>
+        <h2 className="mx-auto text-2xl text-white text-center font-nuni font-medium mt-8 w-60">
+          The place where you will find friends to chat with
+        </h2>
         <div className="flex justify-center py-24 drop-shadow-2xl">
-      <button className="   text-2xl text-white bg-page font-nuni font-black  border rounded-2xl py-6 px-32 border-none"><Link to="registration">Get Started</Link></button>
+          <button className="   text-2xl text-white bg-page font-nuni font-black  border rounded-2xl py-6 px-32 border-none">
+            <Link to="registration">Get Started</Link>
+          </button>
+        </div>
       </div>
-      </div>
-     
-     
     </div>
   );
 };
